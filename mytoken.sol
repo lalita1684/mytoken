070a88a3d31c82af046c6f634f6df839441061ab
lalita1684 <150646002+lalita1684@users.noreply.github.com>
2023-11-13 10:43:29 +0530

    Create mytoken.sol

---
+++ mytoken.sol
@@ -0,0 +1,21 @@
+# My Token
+
+This Solidity code serves as a straightforward example for crafting a custom token, showcasing essential features like minting and burning. Its aim is to help novice Solidity learners grasp the fundamental concepts of the programming language.
+
+## Description
+
+This Solidity script is a basic contract designed for the Ethereum blockchain, employing the Solidity programming language. It includes two functions: "mint," which generates tokens for a designated user, and "burn," responsible for eliminating tokens for the user initiating the action.
+
+## Getting Started
+
+### Executing program
+
+To run this program, you can use Remix, an online Solidity IDE. To get started, go to the Remix website at https://remix.ethereum.org/.
+
+Once you are on the Remix website, create a new file by clicking on the "+" icon in the left-hand sidebar. Save the file with a .sol extension (e.g., mytoken.sol). Copy and paste the code from [mytoken](mytoken.sol)
+
+To compile the code, click on the "Solidity Compiler" tab in the left-hand sidebar. Make sure the "Compiler" option is set to "0.8.18" (or another compatible version), and then click on the "Compile mytoken.sol" button.
+
+Once the code is compiled, you can deploy the contract by clicking on the "Deploy & Run Transactions" tab in the left-hand sidebar. Select the "mytoken" contract from the dropdown menu, and then click on the "Deploy" button.
+
+Once the contract is deployed, you can interact with it by calling the different functions present in it.
